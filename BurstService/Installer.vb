c383e8227078e007ba67091222a5f3dc4d48326c
Quibus <31744573+Quibus-burst@users.noreply.github.com>
2017-09-28 07:47:22 +0200

    First.

---
+++ BurstService/Installer.vb
@@ -0,0 +1,31 @@
+﻿Imports System.ServiceProcess
+Imports System.ComponentModel
+Imports System.Configuration.Install
+
+
+<RunInstaller(True)> Public Class prjInstaller
+    Inherits Installer
+
+    Public Sub New()
+        MyBase.New()
+        InitializeComponent()
+    End Sub
+
+    Friend WithEvents process As ServiceProcessInstaller
+    Friend WithEvents serviceAdmin As ServiceInstaller
+    Private components As Container
+
+    <DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.process = New ServiceProcessInstaller()
+        Me.process.Account = ServiceAccount.LocalSystem
+
+        Me.serviceAdmin = New ServiceInstaller()
+        Me.serviceAdmin.StartType = ServiceStartMode.Automatic
+        Me.serviceAdmin.ServiceName = "Burst Service"
+        Me.serviceAdmin.DisplayName = "Burst Service"
+        Me.Installers.AddRange(New Installer() {Me.process, Me.serviceAdmin})
+    End Sub
+
+
+
+End Class
